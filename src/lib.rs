3a0dda4b8aac0142ef4c2d1b50c8bf9a4b390462
luksab <lukas@sabatschus.de>
2022-04-01 19:08:18 +0200

    partial decoder, encoder

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,142 @@
+#![feature(mixed_integer_ops)]
+
 mod structs;
 use structs::*;
 
+fn get_header(width: u32, height: u32, channels: Channels, colorspace: ColorSpace) -> [u8; 14] {
+    let mut header = [0; 14];
+    header[0] = "qoif".as_bytes()[0];
+    header[1] = "qoif".as_bytes()[1];
+    header[2] = "qoif".as_bytes()[2];
+    header[3] = "qoif".as_bytes()[3];
+    header[4] = (width >> 24) as u8;
+    header[5] = (width >> 16) as u8;
+    header[6] = (width >> 8) as u8;
+    header[7] = width as u8;
+    header[8] = (height >> 24) as u8;
+    header[9] = (height >> 16) as u8;
+    header[10] = (height >> 8) as u8;
+    header[11] = height as u8;
+    header[12] = channels as u8;
+    header[13] = colorspace as u8;
+    header
+}
+
+pub fn encode(pixels: &[Pixel], width: u32, height: u32) -> Vec<u8> {
+    let mut hash = QOIHash::new();
+    let mut encoded = Vec::from(get_header(width, height, Channels::RGB, ColorSpace::SRGB));
+    let mut previous = Pixel::default();
+
+    let num_pixels = pixels.len();
+    assert_eq!(num_pixels, width as usize * height as usize);
+
+    let mut i = 0;
+    loop {
+        if i >= num_pixels {
+            break;
+        }
+        let pixel = pixels[i];
+        if i + 1 < num_pixels && pixels[i + 1] == pixel {
+            hash.insert(pixel);
+            // start run of same color
+            let mut num_same = 0; // start with offset, as spec
+            loop {
+                if num_same == 62 {
+                    break;
+                }
+                if i + num_same + 1 < num_pixels && pixels[i + num_same + 1] == pixel {
+                    num_same += 1;
+                    hash.insert(pixel);
+                } else {
+                    break;
+                }
+            }
+            i = i + num_same + 1;
+            encoded.push(OpRun::new(num_same as u8).get_encoding());
+        } else {
+            let pix_enc = OpRGB::new(pixel.r, pixel.g, pixel.b).get_encoding();
+            encoded.extend_from_slice(&pix_enc);
+        }
+    }
+    encoded
+}
+
+pub fn decode(encoded: &[u8]) -> Vec<Pixel> {
+    let mut decoded = Vec::new();
+    let mut hash = QOIHash::new();
+    let mut previous = Pixel::default();
+
+    let mut i = 0; // which byte in encoded
+    loop {
+        if i >= encoded.len() {
+            break;
+        }
+        let op = Chunk::from_encoding(&encoded[i..i + 4]);
+        match op {
+            Chunk::RGB(rgb) => {
+                let pixel = Pixel {
+                    r: rgb.r,
+                    g: rgb.g,
+                    b: rgb.b,
+                    a: 255,
+                };
+                hash.insert(pixel);
+                previous = pixel;
+                decoded.push(pixel);
+                i += 4;
+            }
+            Chunk::RGBA(rgba) => {
+                let pixel = Pixel {
+                    r: rgba.r,
+                    g: rgba.g,
+                    b: rgba.b,
+                    a: rgba.a,
+                };
+                hash.insert(pixel);
+                previous = pixel;
+                decoded.push(pixel);
+                i += 5;
+            }
+            Chunk::Index(index) => {
+                let pixel = hash.get(index.index);
+                // hash does not need to be updated
+                previous = pixel;
+                decoded.push(pixel);
+                i += 1;
+            }
+            Chunk::Diff(diff) => {
+                let diff = diff.get_diffs();
+                let pixel = Pixel {
+                    r: previous.r.overflowing_add_signed(diff.0).0,
+                    g: previous.g.overflowing_add_signed(diff.1).0,
+                    b: previous.b.overflowing_add_signed(diff.2).0,
+                    a: previous.a,
+                };
+                hash.insert(pixel);
+                previous = pixel;
+                decoded.push(pixel);
+                i += 1;
+            }
+            Chunk::Luma(luma) => {
+                let diff = luma.get_diffs();
+                let pixel = Pixel {
+                    r: previous.r.overflowing_add_signed(diff.0).0,
+                    g: previous.g.overflowing_add_signed(diff.1).0,
+                    b: previous.b.overflowing_add_signed(diff.2).0,
+                    a: previous.a,
+                };
+                hash.insert(pixel);
+                previous = pixel;
+                decoded.push(pixel);
+                i += 1;
+            }
+            Chunk::Run(run) => {
+                for _ in 0..run.run {
+                    decoded.push(previous);
+                }
+                i += 1;
+            }
+        }
+    }
+    decoded
+}
--- src/structs.rs
+++ src/structs.rs
@@ -1,15 +1,15 @@
 #[repr(u8)]
-enum Channels {
+pub(crate) enum Channels {
     RGB = 3,
     RGBA = 4,
 }
 
-enum ColorSpace {
+pub(crate) enum ColorSpace {
     SRGB = 0,
     Linear = 1,
 }
 
-struct QoiHeader {
+pub(crate) struct QoiHeader {
     magic: [u8; 4],
     width: u32,
     height: u32,
@@ -17,12 +17,12 @@ struct QoiHeader {
     color_space: ColorSpace,
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Pixel {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Pixel {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
 impl Default for Pixel {
@@ -36,30 +36,39 @@ impl Default for Pixel {
     }
 }
 
-struct hash {
+pub(crate) struct QOIHash {
     data: Box<[Pixel]>,
 }
 
-impl hash {
-    pub fn new() -> Self {
-        hash {
+impl QOIHash {
+    pub(crate) fn new() -> Self {
+        QOIHash {
             data: vec![Pixel::default(); 64].into_boxed_slice(),
         }
     }
 
-    fn get_pos(&self, pixel: Pixel) -> usize {
+    pub(crate) fn get_index(&self, pixel: Pixel) -> usize {
         ((pixel.r * 3 + pixel.g * 5 + pixel.b * 11) % 64) as usize
+    }
+
+    pub(crate) fn get(&self, index: u8) -> Pixel {
+        self.data[index as usize]
+    }
+
+    pub(crate) fn insert(&mut self, pixel: Pixel) {
+        let pos = self.get_index(pixel);
+        self.data[pos] = pixel;
     }
 }
 
-struct OpRGB {
-    r: u8,
-    g: u8,
-    b: u8,
+pub(crate) struct OpRGB {
+    pub(crate) r: u8,
+    pub(crate) g: u8,
+    pub(crate) b: u8,
 }
 
 impl OpRGB {
-    fn new(r: u8, g: u8, b: u8) -> Self {
+    pub(crate) fn new(r: u8, g: u8, b: u8) -> Self {
         OpRGB { r, g, b }
     }
 
@@ -72,15 +81,15 @@ impl OpRGB {
     }
 }
 
-struct OpRGBA {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+pub(crate) struct OpRGBA {
+    pub(crate) r: u8,
+    pub(crate) g: u8,
+    pub(crate) b: u8,
+    pub(crate) a: u8,
 }
 
 impl OpRGBA {
-    fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+    pub(crate) fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
         OpRGBA { r, g, b, a }
     }
 
@@ -93,12 +102,12 @@ impl OpRGBA {
     }
 }
 
-struct OpIndex {
-    index: u8,
+pub(crate) struct OpIndex {
+    pub(crate) index: u8,
 }
 
 impl OpIndex {
-    fn new(index: u8) -> Self {
+    pub(crate) fn new(index: u8) -> Self {
         debug_assert!(index < (1 << 6));
         OpIndex { index }
     }
@@ -112,12 +121,12 @@ impl OpIndex {
     }
 }
 
-struct OpDiff {
-    diff: u8,
+pub(crate) struct OpDiff {
+    pub(crate) diff: u8,
 }
 
 impl OpDiff {
-    fn new(r: i8, g: i8, b: i8) -> Self {
+    pub(crate) fn new(r: i8, g: i8, b: i8) -> Self {
         debug_assert!(r >= -2 && r <= 1);
         debug_assert!(g >= -2 && g <= 1);
         debug_assert!(b >= -2 && b <= 1);
@@ -133,15 +142,22 @@ impl OpDiff {
     pub fn get_encoding(&self) -> u8 {
         self.diff | (self.get_tag() << 6)
     }
+
+    pub fn get_diffs(&self) -> (i8, i8, i8) {
+        let r = unsafe { std::mem::transmute::<_, i8>((self.diff >> 4) & 0b11) };
+        let g = unsafe { std::mem::transmute::<_, i8>((self.diff >> 2) & 0b11) };
+        let b = unsafe { std::mem::transmute::<_, i8>((self.diff >> 0) & 0b11) };
+        (r, g, b)
+    }
 }
 
-struct OpLuma {
-    g: u8,
-    rb: u8,
+pub(crate) struct OpLuma {
+    pub(crate) g: u8,
+    pub(crate) rb: u8,
 }
 
 impl OpLuma {
-    fn new(dr: i8, dg: i8, db: i8) -> Self {
+    pub(crate) fn new(dr: i8, dg: i8, db: i8) -> Self {
         debug_assert!(dg >= -32 && dg <= 31);
         debug_assert!(dr - dg >= -8 && dr - dg <= 7);
         debug_assert!(db - dg >= -8 && db - dg <= 7);
@@ -159,14 +175,21 @@ impl OpLuma {
     pub fn get_encoding(&self) -> [u8; 2] {
         [self.get_tag() << 6 | self.g, self.rb]
     }
+
+    pub fn get_diffs(&self) -> (i8, i8, i8) {
+        let g = unsafe { std::mem::transmute::<_, i8>(self.g) };
+        let dr = unsafe { std::mem::transmute::<_, i8>((self.rb >> 4) & 0b11) };
+        let db = unsafe { std::mem::transmute::<_, i8>((self.rb >> 0) & 0b11) };
+        (dr + g, g, db + g)
+    }
 }
 
-struct OpRun {
-    run: u8,
+pub(crate) struct OpRun {
+    pub(crate) run: u8,
 }
 
 impl OpRun {
-    fn new(run: u8) -> Self {
+    pub(crate) fn new(run: u8) -> Self {
         debug_assert!(run < (1 << 6));
         OpRun { run }
     }
@@ -180,11 +203,46 @@ impl OpRun {
     }
 }
 
-enum Chunk {
+pub(crate) enum Chunk {
     RGB(OpRGB),
     RGBA(OpRGBA),
     Index(OpIndex),
     Diff(OpDiff),
     Luma(OpLuma),
     Run(OpRun),
+}
+
+impl Chunk {
+    pub(crate) fn from_encoding(possible_chunk: &[u8]) -> Self {
+        let op = possible_chunk[0];
+        match op & 0b11000000 {
+            0b00 => Chunk::Index(OpIndex::new(op & 0b00111111)),
+            0b01 => Chunk::Diff(OpDiff {
+                diff: op & 0b00111111,
+            }),
+            0b10 => Chunk::Luma(OpLuma {
+                g: op & 0b00111111,
+                rb: possible_chunk[1],
+            }),
+            0b11 => {
+                if op == 0b11111110 {
+                    Chunk::RGB(OpRGB::new(
+                        possible_chunk[1],
+                        possible_chunk[2],
+                        possible_chunk[3],
+                    ))
+                } else if op == 0b11111111 {
+                    Chunk::RGBA(OpRGBA::new(
+                        possible_chunk[1],
+                        possible_chunk[2],
+                        possible_chunk[3],
+                        possible_chunk[4],
+                    ))
+                } else {
+                    Chunk::Run(OpRun::new(op & 0b00111111))
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
 }
