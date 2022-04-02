d0958b283320869051b310fa84dd54079e99373b
luksab <lukas@sabatschus.de>
2022-04-02 16:43:18 +0200

    add more tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
-fn main() {
+use qoi::{decode_to_pix, encode_from_pix, Pixel};
+use rand::Rng;
+
+fn cycling8x8() {
+    let width = 8;
+    let height = 8;
+
+    let pixel_list = (0..10).map(|_| Pixel::random()).collect::<Vec<_>>();
+
+    let pixels = (0..width * height)
+        .map(|i| pixel_list[i % pixel_list.len()])
+        .collect::<Vec<_>>();
+    println!("{:?}", pixels);
+    let encoded = encode_from_pix(&pixels, width as u32, height as u32);
+
+    println!("encoded size: {}", encoded.len());
+    println!("compression ratio: {}", encoded.len() as f32 / (width * height * 4) as f32);
+
+    let decoded = decode_to_pix(&encoded);
+
+    assert_eq!(pixels, decoded);
+}
+
+fn default8x8() {
     let width = 8;
     let height = 8;
     let pixels = vec![qoi::Pixel::default(); width * height];
@@ -10,4 +33,40 @@ fn main() {
 
     assert_eq!(pixels, decoded);
     println!("\n\nyay!\n");
+}
+
+fn random(width: usize, height: usize) {
+    let pixels = (0..width * height)
+        .map(|_| Pixel::random())
+        .collect::<Vec<_>>();
+    // println!("{:?}", pixels);
+    let encoded = encode_from_pix(&pixels, width as u32, height as u32);
+    // println!("encoded: {:?}", &encoded[14..]);
+
+    let decoded = decode_to_pix(&encoded);
+    // println!("decoded: {:?}", decoded);
+
+    if pixels != decoded {
+        println!("{:?}", pixels);
+        println!("{:?}", encoded);
+        println!("{:?}", decoded);
+        panic!("pixels != decoded");
+    }
+    // assert_eq!(pixels, decoded);
+}
+
+fn repeated_random_x_by_x() {
+    let mut rng = rand::thread_rng();
+    for _ in 0..1_000_000 {
+        random(rng.gen_range(1..100), rng.gen_range(1..100));
+        // println!("\n\n\n\n\n\n\n\n");
+        // println!(
+        //     "---------------------------------------------------------------------------------"
+        // );
+    }
+}
+
+fn main() {
+    cycling8x8();
+    repeated_random_x_by_x();
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests {
-    use crate::{Pixel, encode_from_pix, decode_to_pix};
+    use rand::Rng;
+
+    use crate::{decode_to_pix, encode_from_pix, Pixel};
 
     #[test]
     fn constant8x8() {
@@ -18,12 +20,31 @@ mod tests {
     fn increasing8x8() {
         let width = 8;
         let height = 8;
-        let pixels = (0..width * height).map(|i| Pixel {
-            r: i as u8,
-            g: i as u8,
-            b: i as u8,
-            a: 255,
-        }).collect::<Vec<_>>();
+        let pixels = (0..width * height)
+            .map(|i| Pixel {
+                r: i as u8,
+                g: i as u8,
+                b: i as u8,
+                a: 255,
+            })
+            .collect::<Vec<_>>();
+        let encoded = encode_from_pix(&pixels, width as u32, height as u32);
+
+        let decoded = decode_to_pix(&encoded);
+
+        assert_eq!(pixels, decoded);
+    }
+
+    #[test]
+    fn cycling8x8() {
+        let width = 8;
+        let height = 8;
+
+        let pixel_list = (0..10).map(|_| Pixel::random()).collect::<Vec<_>>();
+
+        let pixels = (0..width * height)
+            .map(|i| pixel_list[i % pixel_list.len()])
+            .collect::<Vec<_>>();
         let encoded = encode_from_pix(&pixels, width as u32, height as u32);
 
         let decoded = decode_to_pix(&encoded);
@@ -35,7 +56,9 @@ mod tests {
     fn random4x4() {
         let width = 4;
         let height = 4;
-        let pixels = (0..width * height).map(|_| Pixel::random()).collect::<Vec<_>>();
+        let pixels = (0..width * height)
+            .map(|_| Pixel::random())
+            .collect::<Vec<_>>();
         let encoded = encode_from_pix(&pixels, width as u32, height as u32);
 
         let decoded = decode_to_pix(&encoded);
@@ -44,7 +67,9 @@ mod tests {
     }
 
     fn random(width: usize, height: usize) {
-        let pixels = (0..width * height).map(|_| Pixel::random()).collect::<Vec<_>>();
+        let pixels = (0..width * height)
+            .map(|_| Pixel::random())
+            .collect::<Vec<_>>();
         let encoded = encode_from_pix(&pixels, width as u32, height as u32);
         println!("encoded: {:?}", &encoded[14..]);
 
@@ -56,10 +81,24 @@ mod tests {
 
     #[test]
     fn repeated_random1x1() {
-        loop {
+        for _ in 0..1000 {
             random(1, 1);
             println!("\n\n\n\n\n\n\n\n");
-            println!("---------------------------------------------------------------------------------");
+            println!(
+                "---------------------------------------------------------------------------------"
+            );
+        }
+    }
+
+    #[test]
+    fn repeated_random_x_by_x() {
+        let mut rng = rand::thread_rng();
+        for _ in 0..1_000 {
+            random(rng.gen_range(1..10), rng.gen_range(1..10));
+            println!("\n\n\n\n\n\n\n\n");
+            println!(
+                "---------------------------------------------------------------------------------"
+            );
         }
     }
 }
